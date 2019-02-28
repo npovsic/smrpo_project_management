f4f70372abb31ff9708ba8fbf498b8281d62c8d3
npovsic <npovsic@gmail.com>
2019-02-28 17:03:50 +0100

    First commit

---
+++ api/users/model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const deepPopulate = require('mongoose-deep-populate')(mongoose);
+
+const UserSchema = new Schema({
+    id: ObjectId,
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: String
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
