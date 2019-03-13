73e264ab13c30499645fa113a8d599bbb91c70af
npovsic <npovsic@gmail.com>
2019-03-13 17:13:30 +0100

    Fixed middlewares.

--- middlewares/systemAdminNotAuthorized.js
+++ middlewares/systemAdminNotAuthorized.js
@@ -1,5 +1,7 @@
 module.exports = function (req, res, next) {
     if (req.session.userRole === 'system_admin') {
         res.render('unauthorized');
+    } else {
+        next();
     }
 };
--- middlewares/systemUserNotAuthorized.js
+++ middlewares/systemUserNotAuthorized.js
@@ -1,5 +1,7 @@
 module.exports = function (req, res, next) {
     if (req.session.userRole === 'system_user') {
         res.render('unauthorized');
+    } else {
+        next();
     }
 };
